9ce60132baa7f128ac36677793818607843aea62
Tobias Eriksson <erikstob@arcada.fi>
2023-09-04 15:22:43 +0300

    Params och queries

--- server.js
+++ server.js
@@ -8,9 +8,14 @@ app.get("/", (req, res) => {
 
 app.get("/hello/:namn", (req, res) => {
     res.send(`Hello ${req.params.namn}`)
+    // Params t.ex. /hello/tobias
+    console.log(req.params)
+    // Query t.ex. /hello/tobias?foo=bar
+    console.log(req.query)
 })
 
 console.log("Erik ei ummarra!")
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`)
